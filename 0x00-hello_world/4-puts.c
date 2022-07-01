a9fffbae40c29ab5951d76ebe12678500d5e65d2
LOISbarasuene <www.loisblessingpaul@gmail.com>
2022-07-01 12:08:27 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,16 @@
-/**
- *
- *  * main - Entry point
- *
- *   * Return: Always 0 (Success)
- *
- *    /
+#include <stdio.h>
 
+/**
+ * main - This is the main function
+ *
+ * description - the starting point of the program
+ *
+ */
 
 
 int main(void)
 
 {
-
-	 puts("\"Programming is like building a multilingual puzzle");
-
-	   return (0);
-
+	puts("\"programming is like building a multilingual puzzle\n");
+	return (0);
 }
-
-
