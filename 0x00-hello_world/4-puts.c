1368f6845c7123416c85df363f0c78fb90bb4f3c
LOISbarasuene <www.loisblessingpaul@gmail.com>
2022-06-30 21:05:23 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,8 @@
  * Return: Always 0 (Success)
  */
 
-int main (void)
-{
-       puts("\"programming is like building a multilingual puzzle");
-       return(0);
+int main(void)
+{ puts("\"programming is like building a multilingual puzzle");
+return (0);
 
 }
