0d0d117f26cc462cfabd40a4e675b126c92c4b3b
LOISbarasuene <www.loisblessingpaul@gmail.com>
2022-07-12 12:30:54 -0700

    swapping numbers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,13 +2,13 @@
 
 /**
  *swap_int -> given two integers, swap their respective values
- *@a: 80
- *@b: 72
+ *@a: parameter 1
+ *@b: parameter 2
  */
 void swap_int(int *a, int *b)
 {
 int tmp;
-a = 80  b = 72
+
 	tmp = *a;
 	*a = *b;
 	*b = tmp;
