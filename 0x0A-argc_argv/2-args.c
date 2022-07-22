679d0bc82ceb9cc723079ecf3d81b19cb4d3e236
LOISbarasuene <www.loisblessingpaul@gmail.com>
2022-07-22 13:44:27 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,10 +6,11 @@
  * @argv: an array of a command list
  * Return: 0
  */
- * int main(int argc, char *argv[])
+ int main(int argc, char *argv[])
 {
 	int i;
-	for (i = 0; i < argc; i++)
+
+for (i = 0; i < argc; i++)
 	{
 printf("%s\n", argv[i]);
 	}
